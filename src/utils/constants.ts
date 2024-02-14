46a03c3acc1f9621a87459488904e20c63b0d254
fepvenancio <filipeepv@gmail.com>
2024-02-14 09:47:41 +0000

    new deployed addresses

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -9,10 +9,10 @@ export let BIGDECIMAL_ZERO = new BigDecimal(BIGINT_ZERO);
 export let BIGDECIMAL_ONE = toDecimal(BigInt.fromI32(10).pow(18));
 export let BIGDECIMAL_HUNDRED = toDecimal(BigInt.fromI32(10).pow(20));
 export const UNLOCK_HELPER_ADDRESS = Address.fromString('0xd186e00f3ad162c0f2b44e7fdc2c5697331a6c5c')
-export const UNLOCK_MARKET_ADDRESS = Address.fromString('0x41dda08f36afdb83999e1f03eea87d1b24d31273') // 5
-export const UNLOCK_ACTION_ADDRESS = Address.fromString('0x85ac071604d181d6b051f763b55e93a6c97d7995') // 3
-export const UNLOCK_PROTOCOL_OWNER_ADDRESS = Address.fromString('0x68961f65a49b00a3c80175b9cf047de1f4b10d10')
-export const UNLOCK_AUCTION_ADDRESS = Address.fromString('0xaa90f94ed8420cfed04b5e3908c69c4675570e4a') // 4
+export const UNLOCK_MARKET_ADDRESS = Address.fromString('0x1e102a18cd7845893dacab5dccc4812222875ddc') // 5
+export const UNLOCK_ACTION_ADDRESS = Address.fromString('0x422938a35504af77d610e2f6a486caa8ac8e4a47') // 3
+export const UNLOCK_PROTOCOL_OWNER_ADDRESS = Address.fromString('0x4f2ef8ebadb354dd701181705d33b3e5ad200a8f')
+export const UNLOCK_AUCTION_ADDRESS = Address.fromString('0x876b57f8c3cb8085502cfbf0e47ca8130ea6c021') // 4
 
 export enum OrderStatus {
     ACTIVE = 0,
